318739262bd3c40da034bfa400bc90f48aee82a6
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2020-07-06 00:26:44 -0300

    pages(home): add first ui look

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,33 @@
 import React from 'react'
-import { Text, Flex } from '@chakra-ui/core'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import { Logo } from '../components/logo'
+import { TextAbout } from '../components/text'
+import { List } from '../components/list'
+import { Card } from '../components/card'
+import { Search } from '../components/search'
 
 const IndexPage = () => (
   <Layout>
     <SEO />
-    <Flex align="center" justify="center" minH="100vh">
-      <Text as="h1" fontSize="4xl" fontWeight="bold" color="purple.300">
-        Me Indica!
-      </Text>
-    </Flex>
+
+    <Logo />
+    <TextAbout />
+    <Search />
+
+    <List>
+      {[1, 2, 3, 4, 5].map(i => (
+        <Card
+          key={i}
+          name="Wallace Oliveira"
+          locale="Maringá/PR"
+          senority="Junior, Pleno"
+          stack="Front-end, React"
+          working
+        />
+      ))}
+    </List>
   </Layout>
 )
 
