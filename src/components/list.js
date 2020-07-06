ca91c1029b393152f019ebceecc9f9a7e1c353b3
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2020-07-06 00:14:27 -0300

    components: add list component

---
+++ src/components/list.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Grid } from '@chakra-ui/core'
+
+export function List(props) {
+  return (
+    <Grid
+      gridTemplateColumns="repeat(auto-fit, minmax(288px, 1fr))"
+      columnGap={4}
+      rowGap={8}
+    >
+      {props.children}
+    </Grid>
+  )
+}
