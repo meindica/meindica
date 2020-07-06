d9d6c2aceee47773d69ae884db529b15068c91dc
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2020-07-06 00:26:19 -0300

    components(layout): update layout component to use flex and stack

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,14 +5,20 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 import React from 'react'
-import { ThemeProvider, CSSReset } from '@chakra-ui/core'
+import { ThemeProvider, CSSReset, Flex, Stack } from '@chakra-ui/core'
 import PropTypes from 'prop-types'
 
 const Layout = ({ children }) => {
   return (
     <React.Fragment>
       <CSSReset />
-      <ThemeProvider>{children}</ThemeProvider>
+      <ThemeProvider>
+        <Flex py={[8, 16]} px={[4, 8]} maxW="1440px" marginX="auto">
+          <Stack spacing={[8, 16]} width="100%">
+            {children}
+          </Stack>
+        </Flex>
+      </ThemeProvider>
     </React.Fragment>
   )
 }
