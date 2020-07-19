0e2324f3f316f4f5a375f64e73c373abee5ba4a4
Wallace Batista <wallacebatistaoliveira@gmail.com>
2020-07-19 11:18:04 -0300

    Adiciona o novo título (#23)
    * page(home): increase page padding y
    
    * components: add title component
    
    * components: add light~dark mode button

---
+++ src/components/title.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Text, useColorMode, useTheme } from '@chakra-ui/core'
+
+export function Title({ children, ...props }) {
+  const { colorMode } = useColorMode()
+  const theme = useTheme()
+
+  const shadow =
+    colorMode === 'dark' ? theme.colors.gray[800] : theme.colors.white
+
+  return (
+    <Text
+      as="h2"
+      color="blue.100"
+      fontSize={['5xl', '6xl']}
+      fontWeight="bolder"
+      lineHeight="1"
+      textShadow={`-4px -4px 0 ${shadow}, -5px -5px 0 #696A99`}
+      {...props}
+    >
+      {children}
+    </Text>
+  )
+}
