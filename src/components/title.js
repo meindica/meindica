8b5a954d0dc7c0062dd7c73be470e1c38ef22308
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2020-07-25 19:03:23 -0300

    title: fix colors toggle

--- src/components/title.js
+++ src/components/title.js
@@ -1,8 +1,11 @@
 import React from 'react'
-import { Text, useTheme } from '@chakra-ui/core'
+import { Text, useTheme, useColorMode } from '@chakra-ui/core'
 
 export function Title({ children, ...props }) {
+  const { colorMode } = useColorMode()
   const theme = useTheme()
+
+  const color = { light: theme.colors.blue[200], dark: theme.colors.blue[100] }
 
   return (
     <Text
@@ -11,8 +14,8 @@ export function Title({ children, ...props }) {
       fontSize={['5xl', '6xl']}
       fontWeight="bolder"
       lineHeight="1"
-      style={{ WebkitTextStroke: `2px ${theme.colors.blue[200]}` }}
-      textShadow={`5px 5px 0 ${theme.colors.blue[200]}`}
+      style={{ WebkitTextStroke: `2px ${color[colorMode]}` }}
+      textShadow={`5px 5px 0 ${color[colorMode]}`}
       {...props}
     >
       {children}
