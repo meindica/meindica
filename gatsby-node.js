03fb8a39c8274053c88b304029d4f94b97d796fa
Rita Lino <rita.rlino@gmail.com>
2021-04-18 21:52:23 -0300

    feat: create a file containing all data from sheet

--- gatsby-node.js
+++ gatsby-node.js
@@ -40,21 +40,36 @@ exports.createPages = async ({ graphql, actions }) => {
 
     fs.writeFileSync(
       path.resolve(DIR, `${page}.json`),
-      JSON.stringify({
-        nodes: persons.slice(begin, end),
-        pageInfo: { hasNextPage: page < pagesCount }
-      }, null, 2)
+      JSON.stringify(
+        {
+          nodes: persons.slice(begin, end),
+          pageInfo: { hasNextPage: page < pagesCount },
+        },
+        null,
+        2
+      )
     )
   })
 
+  fs.writeFileSync(
+    path.resolve(DIR, `all.json`),
+    JSON.stringify(
+      {
+        nodes: persons,
+      },
+      null,
+      2
+    )
+  )
+
   const initialData = {
     nodes: persons.slice(0, 8),
-    pageInfo: { hasNextPage: 1 < pagesCount, totalCount }
+    pageInfo: { hasNextPage: 1 < pagesCount, totalCount },
   }
 
   createPage({
     path: '/',
     component: path.resolve('./src/templates/home.js'),
-    context: initialData
+    context: initialData,
   })
 }
